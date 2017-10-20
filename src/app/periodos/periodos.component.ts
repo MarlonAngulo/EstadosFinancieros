18450900ba1b9b34856573f78e3fc69e30420f9d
Juan Gamboa <gamabasis@gmail.com>
2017-10-20 12:37:41 -0600

    Permite cambiar / por -

--- src/app/periodos/periodos.component.ts
+++ src/app/periodos/periodos.component.ts
@@ -9,17 +9,21 @@ import { SalesService } from '../services/sales.service';
 })
 export class periodosComponent implements OnInit {
   costoVentas;
-
+  f1:String;
+  f2:String;
   constructor(
     public _global: AppGlobals,
     private salesService: SalesService
   ) { }
 
   ngOnInit() {
-
-    this.salesService.getAllSales('2015-01-01', '2016-01-01').subscribe(data => {
-      console.log(data);
-      this.costoVentas = data.totalCostoVentas; // Assign array to use in HTML
+    this.f1 = "2015/01/01";
+    this.f1 = this.f1.replace(/\//g, "-");
+    this.f2 = "2016/01/01";
+    this.f2 = this.f2.replace(/\//g, "-");
+    this.salesService.getAllSales(this.f1, this.f2).subscribe(data => {
+      this.costoVentas = data[0].totalCostoVentas; // Assign array to use in HTML
+      console.log(this.costoVentas);
     });
 
 
